9f6b2df9c6be6bff324a31e044a45567ef3e1c53
Clark1945 <dmc950213@gmail.com>
2022-03-06 21:13:59 +0800

    建立資料庫

---
+++ EX6/models/articleModel.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ex6-5',{useNewUrlParser:true});
+
+var articleSchema = new mongoose.Schema({
+    name:String,
+    account:String,
+    type:String,
+    title:String,
+    content:String,
+    like:Array,
+    Comment:Array,
+    postdate:Date
+});
+articleSchema.set('collection','article');
+var model = mongoose.model("article",articleSchema); //member 為模組名稱
+module.exports = model;
