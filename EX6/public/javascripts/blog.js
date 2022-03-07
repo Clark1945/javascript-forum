171e48ca92df230d939ef6083ea92826a3fc8691
Clark1945 <dmc950213@gmail.com>
2022-03-07 20:52:47 +0800

    新增newArticle方法

---
+++ EX6/public/javascripts/blog.js
@@ -0,0 +1,47 @@
+if(!$.cookie('userID') || $.cookie('userID')=="null"){
+    alert("請先登入會員。");
+    location.href="/public/login.html";
+}
+function newarticle(data){
+    var content=document.createElement('tr');
+    content.className='row100 body';
+
+    var dat=new Date(data.postdate);
+    var crt_date=(dat.getMonth()+1)+'/'+dat.getDate()+' '+(dat.getHours() < 10 ? '0'+dat.getHours() : dat.getHours())+ ':'+
+    (dat.getMinutes()<10 ? '0'+dat.getMinutes() : dat.getMinutes());
+
+    var addHtml= `
+    <td class="cell100 column1">
+    <a href="/public/blog.html?type=${data.type}">${data.type}</a></td>
+    <td class="cell100 column2">
+    <a href="/public/blog.html?type=${data._id}">${data.title}</a></td>
+    <td class="cell100 column3">
+    ${data.like.length}</td>
+    <td class="cell100 column4">
+    ${data.comment.length}</td>
+    <td class="cell100 column5">
+    <a href="/public/blog.html?account=${data.account}">${data.account}</a></td>
+    <td class="cell100 column6">
+    <a href="/public/blog.html?postdate=${crt_date}">${crt_date}</a></td>
+    `;
+
+    content.insertAdjacentHTML('beforeend',addHtml);
+    $('#content').append(content);
+}
+function getUrlVal(val){
+    var query=window.location.search.substring(1);
+    var vars=query.split("&");
+    for(var i =0;i<vars.length;i++){
+        var pair=vars[i].split("=");
+        if(pair[0]==val){
+            return pair[1];//回傳值
+        }
+    }
+    return (false);
+}
+function search(){
+    location.href='/public/blog.html?title='+$('#title').val();
+}
+function changeType(){
+    
+}
